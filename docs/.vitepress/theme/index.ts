0e7400f5bcf8cd3c021d5f983ba497cbe4592c56
winches <329487092@qq.com>
2023-05-04 16:38:39 +0800

    添加AI网站

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,12 @@
 import Theme from 'vitepress/theme'
+import { global } from '../../src/components/global'
 import './css/index.css'
 
 export default {
   ...Theme,
+  enhanceApp: ({ app }) => {
+    global.forEach(([compName, comp]) => {
+      app.component(compName, comp)
+    })
+  },
 }
---
+++ docs/src/components/global.ts
@@ -0,0 +1,5 @@
+import ShowPicture from './Picture.vue'
+
+export const global = [
+  ['ShowPicture', ShowPicture],
+]
