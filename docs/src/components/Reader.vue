ee3428c44b9bd1d5603811939422f2bd7ca01ad3
Zlinni <zlinni@peropero.net>
2023-05-26 19:56:59 +0800

    feat epub阅读器(#15)
    * feat epub阅读器
    
    * chore: adjust
    
    * chore: update lock file
    
    * chore: directory rename
    
    ---------
    
    Co-authored-by: winches <329487092@qq.com>

---
+++ docs/src/components/Reader.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { VueReader } from 'vue-reader'
+import { ref } from 'vue'
+
+const bookUrl = ref(null)
+function onFileChange(e) {
+  const file = e.target.files[0]
+  bookUrl.value = file
+}
+// const getRendition = (rendition) => {
+//   rendition.themes.register("custom", {
+//     "*": {
+//       color: "#fff",
+//       "background-color": "#252525",
+//     },
+//     image: {
+//       border: "1px solid red",
+//     },
+//     p: {
+//       "font-family": "Helvetica, sans-serif",
+//       "font-weight": "400",
+//       "font-size": "20px",
+//     },
+//   });
+//   rendition.themes.select("custom");
+// };
+const fileInput = ref(null)
+function chooseFile() {
+  fileInput.value.click()
+}
+</script>
+
+<template>
+  <div>
+    <div class="upload-btn" @click="chooseFile">
+      <input ref="fileInput" type="file" @change="onFileChange">
+      <span>请上传</span>
+    </div>
+    <div style="height: 100vh">
+      <VueReader v-if="bookUrl" :url="bookUrl" />
+    </div>
+  </div>
+</template>
+
+<style>
+.upload-btn {
+  position: relative;
+  width: 200px;
+  height: 50px;
+  background: #18a058;
+  border-radius: 5px;
+  overflow: hidden;
+  display: inline-block;
+  cursor: pointer;
+}
+
+.upload-btn input[type="file"] {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  cursor: pointer;
+}
+
+.upload-btn span {
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  transition: 0.2s;
+}
+
+.upload-btn:hover span {
+  background: rgba(0, 0, 0, 0.1);
+}
+
+.upload-btn svg {
+  width: 30px;
+  height: 30px;
+}
+</style>
---
+++ docs/src/play-epub-reader.md
@@ -0,0 +1,16 @@
+---
+title: 阅读器
+---
+
+# Epub阅读器
+
+<script setup>
+import Reader from './components/Reader.vue'
+</script>
+
+<div>
+<Reader></Reader>
+</div>
+
+
+
