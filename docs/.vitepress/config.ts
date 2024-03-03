9fa40893a29fee29b03bbbe1d54c5e094d5c7fbd
winches <329487092@qq.com>
2024-03-03 21:47:59 +0800

    更新

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -119,7 +119,7 @@ const AUTHOR = [
       { text: '个人作品', link: '/author/works' },
       { text: '面试反问', link: '/author/interview' },
       { text: '面试算法', link: '/author/algorithm' },
-      { text: '面试算法', link: '/author/interview-knowledge' },
+      { text: '面试知识', link: '/author/interview-knowledge' },
     ],
   },
 ]
