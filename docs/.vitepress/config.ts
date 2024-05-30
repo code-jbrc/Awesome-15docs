bd4e425c703ccf85db17c0069aaed6b467f9a5f5
winches <329487092@qq.com>
2024-05-30 10:19:47 +0800

    feat: init monitor page

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -43,6 +43,7 @@ const PROJECT = {
     { text: 'Vite 开发问题', link: '/project/vite' },
     { text: 'AST 学习记录', link: '/project/ast' },
     { text: 'Eslint 配置', link: '/project/eslint' },
+    { text: '前端服务监控SDK', link: '/project/monitor' },
   ],
 }
 
